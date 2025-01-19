9dd794b501cba24ce6c9faa71c5d06a2ecc2660f
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2025-01-20 07:09:34 +0800

    membuat goroutine

---
+++ 03_membuat_goroutine.go
@@ -0,0 +1,9 @@
+package main
+
+/*
+Membuat Goroutine
+- Untuk membuat Goroutine di Go sangatlah sederhana
+- Kita hanya cukup menambahkan perintah go sebelum memanggil function yang akan kita jalankan dalam Goroutine
+- Saat sebuah function kita jalankan dalam Goroutine, function tersebut akan berjalan secara asynchronous, artinya tidak akan ditunggu sampai function tersebut selesai
+- Aplikasi akan lanjut berjalan ke kode program selanjutnya tanpa menunggu Goroutine yang kita buat selesai
+*/
