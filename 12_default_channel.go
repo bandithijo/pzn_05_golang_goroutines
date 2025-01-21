6830427812e43d099440c2318108e05897c2d04b
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2025-01-22 04:20:12 +0800

    default channel

---
+++ 12_default_channel.go
@@ -0,0 +1,9 @@
+package main
+
+/*
+Default Channel
+- Apa yang terjadi jika kita melakukan select terhadap Channel yang ternyata tidak ada datanya?
+- Maka kita akan menunggu sampai data ada
+- Kadang mungkin kita ingin melakukan sesuatu jika misal semua Channel tidak ada datanya ketika kita melakukan select Channel
+- Dalam select, kita bisa menambahkan default, dimana ini akan dieksekusi jika memang di semua channel yang kita select tidak ada datanya
+*/
