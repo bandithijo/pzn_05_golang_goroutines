15776d0f3398823a5b6256e5f94aa48e319160b7
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2025-01-25 08:17:41 +0800

    deadlock

---
+++ 16_deadlock.go
@@ -0,0 +1,8 @@
+package main
+
+/*
+Deadlock
+- Hati-hati saat membuat aplikasi yang parallel atau concurrent, masalah yang sering kita hadapi adalah Deadlock
+- Deadlock adalah keadaan dimana sebuah proses Goroutine saling menunggu lock sehingga tidak ada satupun Goroutine yang bisa jalan
+- Sekarang kita coba simulasikan proses deadlock
+*/
