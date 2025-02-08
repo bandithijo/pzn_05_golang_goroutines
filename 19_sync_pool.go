325d6d7b6227ccef7faae50d851446645e47f0f7
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2025-02-08 21:42:19 +0800

    pool

---
+++ 19_sync_pool.go
@@ -0,0 +1,10 @@
+package main
+
+/*
+sync.Pool
+
+Pool
+- Pool adalah implementasi design pattern bernama object pool pattern
+- Sederhananya, design pattern Pool ini digunakan untuk menyimpan data, selanjutnya untuk menggunakan datanya, kita bisa mengambil dari Pool, dan setelah selesai menggunakan datanya, kita bisa menyimpan kembali ke Pool nya
+- Implementasi Pool di Go ini sudah aman dari problem race condition
+*/
