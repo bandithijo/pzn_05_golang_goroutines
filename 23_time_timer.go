c999c35586d9357e05a306173e7f62e94b16d0d4
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2025-02-09 22:31:26 +0800

    timer

---
+++ 23_time_timer.go
@@ -0,0 +1,18 @@
+package main
+
+/*
+time.Timer
+
+- Timer adalah representasi satu kejadian
+- Ketika waktu timer sudah expire, maka event akan dikirim ke dalam channel
+- Untuk membuat Timer kita bisa menggunakan time.NewTimer(duration)
+
+time.After()
+- Kadang kita hanya butuh channel nya saja, tidak membutuhkan data Timer nya
+- Untuk melakukan hal itu, kita bisa menggunakan function time.After(duration)
+
+time.AfterFunc()
+- Kadang ada kebutuhan kita ingin menjalankan sebuah function dengan delay waktu tertentu
+- Kita bisa memanfaatkan Timer dengan menggunakan function timer.AfterFunc()
+- Kita tidak perlu lagi menggunakan channel nya, cukup kirim kan function yang akan dipanggil ketika Timer mengirim kejadiannya
+*/
